ee332bfb51341351d8aee7e4563155023a8bffc0
makkahwi <makkahwi@live.com>
2021-03-05 15:12:24 +0800

    Commit Init

---
+++ assets/map/mapdata.js
@@ -0,0 +1,924 @@
+﻿var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#aaaaaa",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "",
+    state_color: "#cccccc",
+    state_hover_color: "#cccccc",
+    state_url: "",
+    border_size: 0.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#fff",
+    location_opacity: "1",
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "10",
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#fff",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    manual_zoom: ""
+  },
+  state_specific: {
+    AF: {
+      name: "Afghanistan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AO: {
+      name: "Angola",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AL: {
+      name: "Albania",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AE: {
+      name: "United Arab Emirates",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AR: {
+      name: "Argentina",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AU: {
+      name: "Australia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AT: {
+      name: "Austria",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AZ: {
+      name: "Azerbaijan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BI: {
+      name: "Burundi"
+    },
+    BE: {
+      name: "Belgium",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BJ: {
+      name: "Benin"
+    },
+    BF: {
+      name: "Burkina Faso"
+    },
+    BD: {
+      name: "Bangladesh",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BG: {
+      name: "Bulgaria",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BH: {
+      name: "Bahrain",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    BZ: {
+      name: "Belize"
+    },
+    BO: {
+      name: "Bolivia"
+    },
+    BR: {
+      name: "Brazil",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BN: {
+      name: "Brunei Darussalam",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    BT: {
+      name: "Bhutan"
+    },
+    BW: {
+      name: "Botswana"
+    },
+    CF: {
+      name: "Central African Republic"
+    },
+    CA: {
+      name: "Canada",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CH: {
+      name: "Switzerland",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CL: {
+      name: "Chile",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CN: {
+      name: "China",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CI: {
+      name: "Côte d'Ivoire"
+    },
+    CM: {
+      name: "Cameroon"
+    },
+    CD: {
+      name: "Democratic Republic of the Congo"
+    },
+    CG: {
+      name: "Republic of Congo"
+    },
+    CO: {
+      name: "Colombia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CR: {
+      name: "Costa Rica",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CU: {
+      name: "Cuba"
+    },
+    CZ: {
+      name: "Czech Republic",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    DE: {
+      name: "Germany",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    DJ: {
+      name: "Djibouti"
+    },
+    DK: {
+      name: "Denmark",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    DO: {
+      name: "Dominican Republic"
+    },
+    DZ: {
+      name: "Algeria",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    EC: {
+      name: "Ecuador"
+    },
+    EG: {
+      name: "Egypt",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ER: {
+      name: "Eritrea"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ET: {
+      name: "Ethiopia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    FI: {
+      name: "Finland",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    FJ: {
+      name: "Fiji",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    GA: {
+      name: "Gabon"
+    },
+    GB: {
+      name: "United Kingdom",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GH: {
+      name: "Ghana",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    GN: {
+      name: "Guinea"
+    },
+    GM: {
+      name: "The Gambia"
+    },
+    GW: {
+      name: "Guinea-Bissau"
+    },
+    GQ: {
+      name: "Equatorial Guinea"
+    },
+    GR: {
+      name: "Greece",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    GL: {
+      name: "Greenland"
+    },
+    GT: {
+      name: "Guatemala",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    GY: {
+      name: "Guyana"
+    },
+    HN: {
+      name: "Honduras"
+    },
+    HR: {
+      name: "Croatia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    HT: {
+      name: "Haiti"
+    },
+    HU: {
+      name: "Hungary",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ID: {
+      name: "Indonesia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    IN: {
+      name: "India",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IR: {
+      name: "Iran",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    IQ: {
+      name: "Iraq",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IL: {
+      name: "Israel",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    IT: {
+      name: "Italy",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    JM: {
+      name: "Jamaica"
+    },
+    JO: {
+      name: "Jordan",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    JP: {
+      name: "Japan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KZ: {
+      name: "Kazakhstan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KE: {
+      name: "Kenya",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KG: {
+      name: "Kyrgyzstan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KH: {
+      name: "Cambodia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KR: {
+      name: "Republic of Korea",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    LA: {
+      name: "Lao PDR",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    LB: {
+      name: "Lebanon",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    LR: {
+      name: "Liberia"
+    },
+    LY: {
+      name: "Libya",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    LK: {
+      name: "Sri Lanka",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    LS: {
+      name: "Lesotho"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar"
+    },
+    MX: {
+      name: "Mexico",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    MK: {
+      name: "Macedonia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ML: {
+      name: "Mali"
+    },
+    MM: {
+      name: "Myanmar",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia"
+    },
+    MZ: {
+      name: "Mozambique"
+    },
+    MR: {
+      name: "Mauritania",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    MW: {
+      name: "Malawi",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    MY: {
+      name: "Malaysia",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    NA: {
+      name: "Namibia",
+      inactive: "yes"
+    },
+    NE: {
+      name: "Niger"
+    },
+    NG: {
+      name: "Nigeria",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    NI: {
+      name: "Nicaragua"
+    },
+    NL: {
+      name: "Netherlands",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    NO: {
+      name: "Norway",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    NP: {
+      name: "Nepal",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    NZ: {
+      name: "New Zealand",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    OM: {
+      name: "Oman",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    PK: {
+      name: "Pakistan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    PA: {
+      name: "Panama"
+    },
+    PE: {
+      name: "Peru",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    PH: {
+      name: "Philippines",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    PG: {
+      name: "Papua New Guinea"
+    },
+    PL: {
+      name: "Poland",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    RO: {
+      name: "Romania",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    RU: {
+      name: "Russia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    RW: {
+      name: "Rwanda"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    SD: {
+      name: "Sudan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    SS: {
+      name: "South Sudan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    SN: {
+      name: "Senegal"
+    },
+    SL: {
+      name: "Sierra Leone"
+    },
+    SV: {
+      name: "El Salvador"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    SZ: {
+      name: "Swaziland"
+    },
+    SY: {
+      name: "Syria",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    TD: {
+      name: "Chad"
+    },
+    TG: {
+      name: "Togo"
+    },
+    TH: {
+      name: "Thailand",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    TR: {
+      name: "Turkey",
+      hover_color: "#d00000",
+      color: "#6b0000"
+    },
+    TW: {
+      name: "Taiwan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    TZ: {
+      name: "Tanzania"
+    },
+    UG: {
+      name: "Uganda"
+    },
+    UA: {
+      name: "Ukraine",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    UY: {
+      name: "Uruguay"
+    },
+    US: {
+      name: "United States",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    UZ: {
+      name: "Uzbekistan",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    VE: {
+      name: "Venezuela",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    VN: {
+      name: "Vietnam",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    VU: {
+      name: "Vanuatu",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    YE: {
+      name: "Yemen",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ZA: {
+      name: "South Africa",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ZM: {
+      name: "Zambia",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ZW: {
+      name: "Zimbabwe"
+    },
+    SO: {
+      name: "Somalia"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    ES: {
+      name: "Spain",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    CV: {
+      name: "Cape Verde"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "São Tomé and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga",
+      hover_color: "#d00000",
+      color: "#d00000"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus"
+    },
+    RE: {
+      name: "Reunion (France)"
+    },
+    YT: {
+      name: "Mayotte (France)"
+    },
+    MQ: {
+      name: "Martinique (France)"
+    },
+    GP: {
+      name: "Guadeloupe (France)"
+    },
+    CW: {
+      name: "Curaco (Netherlands)"
+    },
+    IC: {
+      name: "Canary Islands (Spain)"
+    }
+  },
+  locations: {
+    paris: {
+      name: "Kuala Lumpur, Malaysia",
+      lat: "3.139003",
+      lng: "101.686852",
+      description: "2011 - 2020 | Bachelor Study"
+    }
+  },
+  labels: {},
+  regions: {}
+};
