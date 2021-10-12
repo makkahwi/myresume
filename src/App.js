608ebb5893ed4b39c693c0d287af4c73d7aa63e4
makkahwi <suhaibahmadai@hotmail.com>
2021-10-12 13:00:43 +0300

    Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 import { useTranslation } from "react-i18next";
 import './App.css';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Nav from './Components/Nav';
-import Portfolio from './Components/Portfolio';
-import Resume from './Components/Resume';
 import Data from './resumeData';
+
+const Nav = lazy(() => import('./Components/Nav'));
+const Header = lazy(() => import('./Components/Header'));
+const About = lazy(() => import('./Components/About'));
+const Resume = lazy(() => import('./Components/Resume'));
+const Portfolio = lazy(() => import('./Components/Portfolio'));
+const Contact = lazy(() => import('./Components/Contact'));
+const Footer = lazy(() => import('./Components/Footer'));
 
 export default function App() {
   const { i18n } = useTranslation();
@@ -45,13 +46,15 @@ export default function App() {
 
   return (
     <div className="App">
-      <Nav />
-      <Header data={Data.main} jobsList={jobsList} setJob={setJob} />
-      <About data={about} />
-      <Resume data={resume} educations={Data.resume.educationList} />
-      <Portfolio data={portfolio} />
-      <Contact data={Data.main} />
-      <Footer data={Data.main} />
+      <Suspense fallback={<div className="spinner"></div>}>
+        <Nav />
+        <Header data={Data.main} jobsList={jobsList} setJob={setJob} />
+        <About data={about} />
+        <Resume data={resume} educations={Data.resume.educationList} />
+        <Portfolio data={portfolio} />
+        <Contact data={Data.main} />
+        <Footer data={Data.main} />
+      </Suspense>
     </div>
   );
 }
