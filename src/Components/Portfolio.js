8ebf8f9be1c3be8bb24a3d2ade3593a9a5f17ad5
makkahwi <suhaibahmadai@hotmail.com>
2021-10-15 17:05:10 +0300

    Minor Fixes

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -32,17 +32,15 @@ export default function Portfolio({ data }) {
         <div className="twelve columns collapsed">
           <h1 className={`${i18n.language === "ar" && "arabic"}`}>{t("Samples of My Work")}</h1>
           {filters.length > 0 && (
-            <div className="row">
-              <div className="twelve columns main-col text-center text-underline">
-                <div className={`${filtersLength(filters.length + 1)} columns main-col`}>
-                  <p><a className={`button buttonWidth ${!filter && "selected"}`} onClick={() => setFilter()}>{t("All")}</a></p>
-                </div>
-                {filters.map((filt, i) => (
-                  <div key={i} className={`${filtersLength(filters.length + 1)} columns main-col`}>
-                    <p><a className={`button buttonWidth ${filter === filt && "selected"}`} onClick={() => setFilter(filt)}>{t(filt)}</a></p>
-                  </div>
-                ))}
+            <div className="twelve columns main-col text-center text-underline">
+              <div className={`${filtersLength(filters.length + 1)} columns main-col`}>
+                <p><a className={`button buttonWidth ${!filter && "selected"}`} onClick={() => setFilter()}>{t("All")}</a></p>
               </div>
+              {filters.map((filt, i) => (
+                <div key={i} className={`${filtersLength(filters.length + 1)} columns main-col`}>
+                  <p><a className={`button buttonWidth ${filter === filt && "selected"}`} onClick={() => setFilter(filt)}>{t(filt)}</a></p>
+                </div>
+              ))}
             </div>
           )}
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
