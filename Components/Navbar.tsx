482df0a438182900627c73436db974e909368247
makkahwi <suhaibahmadai@hotmail.com>
2023-05-23 13:11:12 +0300

    Minor Code Fix

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 const Navbar = () => {
   const navLinks = [
-    { text: "Home" },
-    { text: "About" },
-    { text: "Resume" },
-    { text: "Works" },
-    { text: "Contacts" },
-    { text: "Lang" },
+    { text: "Home", link: "/" },
+    { text: "About", link: "/about" },
+    { text: "Resume", link: "/resume" },
+    { text: "Portfolio", link: "/portfolio" },
+    { text: "Contacts", link: "/contacts" },
+    { text: "Lang", link: "#" },
   ];
 
   return (
     <div className="row g-0 h-100">
-      {navLinks.map(({ text }, i) => (
+      {navLinks.map(({ text, link }, i) => (
         <Link
           className={`col-md-12 col-2 text-white text-center text-decoration-none py-3
                         ${i % 2 === 0 ? "bg-dark" : "bg-primary"}`}
-          href="/"
+          href={link}
           key={i}
         >
           {text}
