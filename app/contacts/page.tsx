d95957644ba558616d904a7c2565f012e020a135
makkahwi <suhaibahmadai@hotmail.com>
2023-05-23 19:25:28 +0300

    Build Conact Page Contents

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,8 +1,24 @@
+import PageLayout from "@/Components/PageLayout";
+import data from "@/resumeData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Contacts = () => {
+  const socials = [...data.main.socialList, ...data.links.web];
+
   return (
-    <main>
-      <div className="text-center">Contacts is Here</div>
-    </main>
+    <PageLayout title={"My Contacts"}>
+      {socials.map((social, i) => (
+        <div className="row px-2 align-items-center" key={i}>
+          {social.map(({ icon, url, text }, y) => (
+            <a href={url} target="_blank" className="col-2 text-theme" key={y}>
+              <FontAwesomeIcon icon={icon} />
+            </a>
+          ))}
+
+          <span className="col text-theme">{social[0].text}</span>
+        </div>
+      ))}
+    </PageLayout>
   );
 };
 
