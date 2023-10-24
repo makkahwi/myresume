15fecf26548daf8921bfe8d30b734931e86fc0e2
makkahwi <suhaibahmadai@hotmail.com>
2023-10-24 13:20:04 +0300

    Update AboutSection

--- sections/home/about.jsx
+++ sections/home/about.jsx
@@ -4,19 +4,12 @@ import Typography from "components/typography";
 const AboutSection = () => {
   return (
     <PageSection color="warning">
-      <Typography size={3} justify="center" className="p-5">
-        With many years of experience and passion about building digital
-        solutions, and with high interests into education sector & its
-        challenges, I'm all about dedicating myself to overcome those issues
-        with availability & endless development of skills & experience.
-      </Typography>
-
-      <Typography size={3} justify="center" className="p-5">
+      <Typography size={4} justify="center" className="p-5">
         Passionate entrepreneur with a vision to revolutionize the edtech
         industry. Founder and CEO of Semesteer, dedicated to creating
         cutting-edge solutions to digitize schools education. Bringing 8 years
-        of experience in web development, corporate world and proven track
-        record in bringing new products to market.
+        of experience in IT jobs, corporate world and proven track record in
+        bringing new products to market.
       </Typography>
     </PageSection>
   );
