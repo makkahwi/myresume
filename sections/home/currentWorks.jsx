674eff7f68a6b280328b0cdc06d3bbc0f466220c
makkahwi <suhaibahmadai@hotmail.com>
2023-10-31 13:25:38 +0300

    Update CurrentWorks Content

--- sections/home/currentWorks.jsx
+++ sections/home/currentWorks.jsx
@@ -4,9 +4,24 @@ import { Col, Row } from "reactstrap";
 
 const CurrentWorksSection = () => {
   const products = [
-    { name: "Semesteer", link: "https://www.semesteer.com/" },
-    { name: "TeamsLine", link: "http://teamsline.com/" },
-    { name: "Learngine", link: "https://www.alembicsoft.com/" },
+    {
+      name: "Semesteer",
+      link: "https://www.semesteer.com/",
+      desc: "An online system that manages students data, and makes the most of it by extensive statistical analysis and planned AI-based data analysis. Product's V3 was built and currently under testing before soft-launching to Jordan's schools.",
+      logo: "/images/semesteer.png",
+    },
+    {
+      name: "TeamsLine",
+      link: "http://www.teamsline.com/",
+      desc: "A web app to manage school staff and provide the needs of HR managment, with option of integrating the same users database of Semesteer. MVP was built already and currently being tested by a client.",
+      logo: "/images/teamsline.png",
+    },
+    {
+      name: "Learngine",
+      link: "https://www.alembicsoft.com/",
+      desc: "A platform that will be integratable with Semesteer to manage students learning materials provided by schools. The MVP is currently under construction and an interested client is waiting to start validating and testing.",
+      logo: "/images/learngine.png",
+    },
   ];
 
   return (
@@ -17,7 +32,7 @@ const CurrentWorksSection = () => {
       id="works"
     >
       <Row>
-        {products.map(({ name, link }, i) => (
+        {products.map(({ name, link, desc, logo }, i) => (
           <Col md={4} key={i}>
             <Typography
               size={3}
@@ -30,8 +45,12 @@ const CurrentWorksSection = () => {
                 target="_blank"
                 className="text-decoration-none text-inherit"
               >
-                {name}
+                <img src={logo} width="40%" />
               </a>
+            </Typography>
+
+            <Typography className="pt-0 px-4" color="white">
+              {desc}
             </Typography>
           </Col>
         ))}
