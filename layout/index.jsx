63bcf4e4b20f6ce862be69d85657462c44bb8fd3
makkahwi <suhaibahmadai@hotmail.com>
2023-10-31 13:22:25 +0300

    Minor UI Fix

--- layout/index.jsx
+++ layout/index.jsx
@@ -1,15 +1,14 @@
 import { Fragment } from "react";
-import Navbar from "./Navbar";
+
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
     <Fragment>
       <Navbar />
 
-      <div className="mx-0 mb-0 p-0 w-100" style={{ marginTop: "11vh" }}>
-        {children}
-      </div>
+      <div className="mx-0 mb-0 p-0 w-100 py-5">{children}</div>
 
       <Footer />
     </Fragment>
