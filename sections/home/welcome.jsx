207aeb0517fb667311616ce5eb0d6b26e12be810
makkahwi <suhaibahmadai@hotmail.com>
2023-11-13 09:06:44 +0300

    Update Welcome Section

--- sections/home/welcome.jsx
+++ sections/home/welcome.jsx
@@ -1,16 +1,10 @@
 import CarouselComp from "components/carousel";
 import PageSection from "components/pageSection";
 import Typography from "components/typography";
-import { useEffect, useState } from "react";
-import ReactPlayer from "react-player";
 import { Col, Row } from "reactstrap";
 
 const WelcomeSection = () => {
-  const SlideComp = ({ text }) => (
-    <Typography size={5} justify="center">
-      {text}
-    </Typography>
-  );
+  const SlideComp = ({ text }) => <Typography size={3}>{text}</Typography>;
 
   const texts = [
     "Founder & CEO of Semesteer, Entrepreneur, Innovator & Web Developer",
@@ -20,17 +14,11 @@ const WelcomeSection = () => {
     "Investing Rechnology Passion & Experience Into Education Solutions",
   ];
 
-  const [showVideo, setShowVideo] = useState(false);
-
-  useEffect(() => {
-    setShowVideo(false);
-  }, []);
-
   return (
-    <PageSection>
-      <Row style={{ minHeight: "70vh" }}>
+    <PageSection color="dark">
+      <Row style={{ minHeight: "70vh" }} className="text-white">
         <Col lg={6} style={{ margin: "auto 0" }} className="py-5">
-          <Typography size={1} justify="center" className="pb-5">
+          <Typography size={1} className="pb-4" style={{ fontSize: "3.5vw" }}>
             Suhaib Ahmad
           </Typography>
 
@@ -42,15 +30,7 @@ const WelcomeSection = () => {
         </Col>
 
         <Col lg={6} style={{ margin: "auto 0" }} className="text-center">
-          {showVideo ? (
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-              light="/images/profile.jpg"
-              width="100%"
-            />
-          ) : (
-            <img src="/images/profile.jpg" width="70%" />
-          )}
+          <img src="/images/profile.jpg" width="70%" />
         </Col>
       </Row>
     </PageSection>
