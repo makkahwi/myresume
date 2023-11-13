e58132640eb7bb28068804376ec219f900f50a0f
makkahwi <suhaibahmadai@hotmail.com>
2023-11-13 13:57:52 +0300

    Update Footer UI

--- layout/Footer.jsx
+++ layout/Footer.jsx
@@ -6,21 +6,29 @@ import { Button, ButtonToolbar, Navbar, NavbarText } from "reactstrap";
 const Footer = () => {
   return (
     <Navbar className="py-4 px-4" color="secondary" dark>
-      <NavbarText>
-        <Typography color="white">
-          All Rights Reserved For Suhaib Ahmad © {new Date().getFullYear()}
-        </Typography>
-      </NavbarText>
+      <NavbarText className="text-center w-100">
+        <img src="/images/logo.png" width="100vw" className="my-5" />
 
-      <ButtonToolbar>
-        {socialLinksList.map(({ icon, link }, i) => (
-          <a href={link} target="_blank" key={i}>
-            <Button color="ghost" outline className="text-white ms-2" size="sm">
-              <FontAwesomeIcon icon={icon} />
-            </Button>
-          </a>
-        ))}
-      </ButtonToolbar>
+        <Typography color="white" justify="center">
+          All Rights Reserved For <b>Suhaib Ahmad</b> ©{" "}
+          {new Date().getFullYear()}
+        </Typography>
+
+        <ButtonToolbar className="justify-content-center my-3">
+          {socialLinksList.map(({ icon, link }, i) => (
+            <a href={link} target="_blank" key={i}>
+              <Button
+                color="ghost"
+                outline
+                className="text-white ms-2"
+                size="sm"
+              >
+                <FontAwesomeIcon icon={icon} />
+              </Button>
+            </a>
+          ))}
+        </ButtonToolbar>
+      </NavbarText>
     </Navbar>
   );
 };
