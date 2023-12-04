3d0ca1263e552f1456c57566a82d4662dabe64c9
makkahwi <suhaibahmadai@hotmail.com>
2023-12-04 15:56:30 +0300

    AlembicSoft => Semesteer

--- sections/home/currentWorks.jsx
+++ sections/home/currentWorks.jsx
@@ -4,58 +4,30 @@ import Link from "next/link";
 import { Button, Col, Row } from "reactstrap";
 
 const CurrentWorksSection = () => {
-  const products = [
-    {
-      name: "Semesteer",
-      link: "https://www.semesteer.com/",
-      desc: "An online system that manages students data, and makes the most of it by extensive statistical analysis and planned AI-based data analysis. Product's V3 was built and currently under testing before soft-launching to Jordan's schools.",
-      logo: "/images/semesteer.png",
-    },
-    {
-      name: "TeamsLine",
-      link: "http://www.teamsline.com/",
-      desc: "A web app to manage school staff and provide the needs of HR managment, with option of integrating the same users database of Semesteer. MVP was built already and currently being tested by a client.",
-      logo: "/images/teamsline.png",
-    },
-    {
-      name: "Learngine",
-      link: "https://www.alembicsoft.com/",
-      desc: "A platform that will be integratable with Semesteer to manage students learning materials provided by schools. The MVP is currently under construction and an interested client is waiting to start validating and testing.",
-      logo: "/images/learngine.png",
-    },
-  ];
-
   return (
     <PageSection
-      title="Innovative Ventures"
-      subtitle="Current Entrepreneurial Pursuits"
+      title="Innovative Venture"
+      subtitle="Current Entrepreneurial Pursuit"
       color="warning"
       id="works"
     >
-      <Row>
-        {products.map(({ name, link, desc, logo }, i) => (
-          <Col md={4} key={i}>
-            <Typography
-              size={3}
-              className="pb-5"
-              color="white"
-              justify="center"
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <Typography size={3} className="pb-5" color="white" justify="center">
+            <a
+              href="https://www.semesteer.com/"
+              target="_blank"
+              className="text-decoration-none text-inherit"
             >
-              <a
-                href={link}
-                target="_blank"
-                className="text-decoration-none text-inherit"
-              >
-                <img src={logo} width="40%" />
-              </a>
-            </Typography>
-          </Col>
-        ))}
+              <img src="/images/semesteer.png" width="40%" />
+            </a>
+          </Typography>
+        </Col>
 
         <Col md={12} className="my-5 text-center">
           <Link href="/projects">
             <Button color="light" className="w-25">
-              Discover Projects
+              Read More
             </Button>
           </Link>
         </Col>
--- sections/projects/projects.jsx
+++ sections/projects/projects.jsx
@@ -3,67 +3,33 @@ import Typography from "components/typography";
 import { Col, Row } from "reactstrap";
 
 const ProjectsSection = () => {
-  const projects = [
-    {
-      logo: "semesteer.png",
-      texts: [
-        "This was the solution I developed within a school, tested it and validated the concept. And going to be the flagship of the projects I did or will start working on.",
-        "It is about centralizing, organizing, offering online, analyzing, informing and alerting all the education process parties of the data updated of the students of a school.",
-        "So a school would acquire the solution, feed it with the initial data of the education process with out help, and then start updating the data, engaging all school staff, all the students and their families.",
-        "This project already gained funds and a team is working on its further development and Go-To-Market plan execution. For all the details, check its website @ https://www.semesteer.com.",
-      ],
-      website: "https://www.semesteer.com/",
-      highlight: true,
-    },
-    {
-      logo: "teamsline-dark.png",
-      texts: [
-        "As Semesteer requested to have all the staff data and have own access to the system, the second most important matter within a school was to organize its HR management, work reporting and all staff-to-staff issues. And that’s how TeamsLine was born.",
-        "TeamsLine is a human resources management system (HRMS), merged with tasks and works reporting system, which will be possible to be integrated with Semesteer to use the same user data of schools staff and management.",
-        "The MVP of it was built already and it is now being tested by a client to validate it and update it before its launch.",
-      ],
-      website: "http://teamsline.com/",
-    },
-    {
-      logo: "learngine.png",
-      texts: [
-        "The third matter to take care of is to help students with their educational journey, even outside school time. So Learngine will help school teachers to feed their students more educational materials to meet their various needs, and even allow them (students) to test and practice and evaluate their progress of academic work.",
-        "This is still on the process of developing it from an idea to an MVP.",
-      ],
-    },
+  const texts = [
+    "I developed and tested this solution within a school environment for a whole year, successfully validating the concept.",
+    "This project is my primary focus, aiming to centralize, organize, and streamline the education process by providing online tools for data analysis, information sharing, and timely alerts to all stakeholders involved.",
+    "The solution is designed to be user-friendly: a school can easily input initial education data independently. Once set up, the platform facilitates ongoing updates, involving school staff, students, and their families.",
+    "With initial funding secured, a dedicated team is actively working on the project's continued development and executing a Go-To-Market plan. For detailed information, please visit the project's website @ https://www.semesteer.com.",
+    "Future plans include expanding the solution to digitze every corner of the education process going on & off-campus.",
   ];
 
-  return projects.map(({ logo, texts, highlight, website }, i) => (
-    <PageSection
-      color={highlight ? "warning" : i % 2 === 0 ? "secondary" : "light"}
-      key={i}
-    >
+  return (
+    <PageSection color="warning">
       <Row className="align-items-center">
-        <Col md={6} className={`text-center order-${i % 2 === 0 ? "1" : "2"}`}>
-          {website ? (
-            <a href={website} target="_blank">
-              <img src={`/images/${logo}`} width="65%" />
-            </a>
-          ) : (
-            <img src={`/images/${logo}`} width="65%" />
-          )}
+        <Col md={6} className="text-center">
+          <a href="https://www.semesteer.com/" target="_blank">
+            <img src="/images/semesteer.png" width="65%" />
+          </a>
         </Col>
 
-        <Col md={6} className={`text-center order-${i % 2 === 0 ? "2" : "1"}`}>
+        <Col md={6} className="text-center">
           {texts.map((text, y) => (
-            <Typography
-              size={4}
-              className="my-5"
-              color={highlight ? "white" : i % 2 === 0 ? "white" : "dark"}
-              key={y}
-            >
+            <Typography size={4} className="my-5" color="white" key={y}>
               {text}
             </Typography>
           ))}
         </Col>
       </Row>
     </PageSection>
-  ));
+  );
 };
 
 export default ProjectsSection;
