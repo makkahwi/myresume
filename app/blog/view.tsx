f67a222054aef2c2ba401ac3e18bc914a5c8138e
makkahwi <suhaibahmadai@hotmail.com>
2023-12-18 17:00:29 +0300

    Remove Redundant GetPosts API Code

--- app/blog/view.tsx
+++ app/blog/view.tsx
@@ -1,28 +1,5 @@
-import { getSemesteerBlogEnPosts } from "@/api";
-import { revalidationTimer } from "@/consts/data";
 import PagesLayout from "@/layout/PagesLayout";
 import BlogSection, { post } from "@/sections/home/blog";
-
-export const getServerSideProps = async () => {
-  let posts: post[] = [];
-
-  await getSemesteerBlogEnPosts().then(
-    (res: { data: post[] }) =>
-      (posts = res.data
-        ?.filter(({ category }) => category === "Articles")
-        .map(({ category, ...rest }) => ({
-          ...rest,
-          category: "Digitizing Education",
-        })))
-  );
-
-  return {
-    props: {
-      posts,
-      revalidate: revalidationTimer,
-    },
-  };
-};
 
 const BlogPage = ({ posts }: { posts: post[] }) => {
   return (
