ea1f43c3977efbc94611d04b33d298c0e314c2f4
makkahwi <suhaibahmadai@hotmail.com>
2024-02-27 22:20:06 +0300

    Update Google Analytics Engagement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
+import ScrollUpButton from "@/components/ScrollUp";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "react-slideshow-image/dist/styles.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import "./style.scss";
-import "./style.css";
-import ScrollUpButton from "@/components/ScrollUp";
-import Head from "next/head";
 import Script from "next/script";
+import "react-slideshow-image/dist/styles.css";
+import "./style.css";
+import "./style.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,20 +20,6 @@ export const metadata: Metadata = {
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html dir="ltr" lang="en">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-WDD6QTCBFJ"
-      />
-
-      <Script id="google-analytics">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-WDD6QTCBFJ');
-          `}
-      </Script>
-
       <head>
         <link rel="icon" href="/FavIcon.jpg" sizes="any" />
       </head>
@@ -41,6 +27,21 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <body className={inter.className}>
         <main>{children}</main>
         <ScrollUpButton />
+
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-WDD6QTCBFJ"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WDD6QTCBFJ');
+          `}
+        </Script>
       </body>
     </html>
   );
