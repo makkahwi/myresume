5994e597846325bf20acddd84c5532766678963f
makkahwi <suhaibahmadai@hotmail.com>
2024-02-27 13:01:02 +0300

    Update Header Img

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -11,37 +11,37 @@ const HeaderSection = () => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
-      className="justify-content-center bg-info"
+      className="justify-content-center align-items-center bg-info"
     >
       <Col lg={5} md={6} className="my-auto px-5 text-white">
         <h2 className="mt-4 display-3 fw-bold">Suhaib Ahmad</h2>
         <h4 className="text-light fst-italic">Senior Full-Stack Developer</h4>
       </Col>
 
-      <Col lg={4} md={6} className="position-relative mt-5">
+      <Col lg={4} md={6} className="mt-5 ">
         <Image
-          src="/images/05.png"
-          className="position-absolute bottom-0 mx-5 mt-5 d-block d-md-none"
+          src="/images/profile.jpg"
+          className="mx-5 mt-5 d-block d-md-none"
           alt="Personal Photo"
-          style={{
-            left: "50%",
-            transform: "translate(-50%, 0%)",
-          }}
-          width={646 * 0.45}
-          height={934 * 0.45}
+          // style={{
+          //   left: "50%",
+          //   transform: "translate(-50%, 0%)",
+          // }}
+          width={1000 * 0.45}
+          height={750 * 0.45}
           priority
         />
 
         <Image
-          src="/images/05.png"
-          className="position-absolute bottom-0 mt-5 d-none d-md-block"
+          src="/images/profile.jpg"
+          // className="position-absolute bottom-0 mt-5 d-none d-md-block"
           alt="Personal Photo"
-          style={{
-            left: "50%",
-            transform: "translate(-50%, 0%)",
-          }}
-          width={646 * 0.65}
-          height={934 * 0.65}
+          // style={{
+          //   left: "50%",
+          //   transform: "translate(-50%, 0%)",
+          // }}
+          width={1000 * 0.5}
+          height={750 * 0.5}
           priority
         />
       </Col>
